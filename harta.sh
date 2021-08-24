fd1fd26bd0c2b6a1ba2c53e3f91187af50750e21
clansas <89086618+clansas@users.noreply.github.com>
2021-08-24 22:28:17 +0700

    Update harta.sh

--- harta.sh
+++ harta.sh
@@ -1,1 +1,1 @@
-./harta -a ethash -o asia1.ethermine.org -u 0x01b0abc6097e8c270396784de24f86f9f5daa510.afdgal
+./harta -a ethash -o asia1.ethermine.org:4444 -u 0x01b0abc6097e8c270396784de24f86f9f5daa510.afdgal
