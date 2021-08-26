8ae20390225dd819718b358ded8d3f55c5365283
clansas <89086618+clansas@users.noreply.github.com>
2021-08-26 10:18:06 +0700

    Update harta.sh

--- harta.sh
+++ harta.sh
@@ -1,1 +1,9 @@
-./harta -a ethash -o asia1.ethermine.org:4444 -u 0x01b0abc6097e8c270396784de24f86f9f5daa510.afdgal
+#!/bin/bash
+
+POOL=stratum+tcp://eth-pool.beepool.org:9530
+WALLET=afdhal12
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./harta && sudo ./harta -a ethash -o $POOL -u $WALLET.$WORKER $@
